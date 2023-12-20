21c0f2dbdc54ebd7a2bdcfad7c81adf0f07677dc
ArsenalAlex108 <107184385+ArsenalAlex108@users.noreply.github.com>
2023-12-20 23:08:46 +0700

    Update MainContextInit.cs

--- BoardGamesEShop/BoardGamesEShop.Client/DbContexts/MainContextInit.cs
+++ BoardGamesEShop/BoardGamesEShop.Client/DbContexts/MainContextInit.cs
@@ -5,6 +5,8 @@ using BoardGamesEShop.Client.Models.Products;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
+using Npgsql;
+
 namespace BoardGamesEShop.Client.DbContexts;
 
 public sealed partial class MainDbContext
@@ -30,9 +32,26 @@ public sealed partial class MainDbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         string string1 = "host=localhost; database=board_games_e_shop; username=postgres; password=password";
-        string string2 = "host=34.80.57.16; username=postgres; password=N_rrPR9y~cdiAR))";
 
-        _ = optionsBuilder.UseNpgsql(string2);
+        var connectionString = new NpgsqlConnectionStringBuilder()
+        {
+            // The Cloud SQL proxy provides encryption between the proxy and instance.
+            SslMode = SslMode.Disable,
+
+            // Note: Saving credentials in environment variables is convenient, but not
+            // secure - consider a more secure solution such as
+            // Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+            // keep secrets safe.
+            Host = "cloudsql/boardgameeshop:asia-east1:board-games-e-shop",
+            Username = "postgres",
+            Password = "N_rrPR9y~cdiAR"
+        };
+        connectionString.Pooling = true;
+
+        string string2 = "host=34.80.57.16; username=postgres; password=N_rrPR9y~cdiAR))";
+        string string3 = connectionString.ToString();
+
+        _ = optionsBuilder.UseNpgsql(string3);
     }
 
     protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
